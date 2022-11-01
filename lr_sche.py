ea642de0baf45cfbb08525745c06d4f9030a44fc
Yansongga <54696448+Yansongga@users.noreply.github.com>
2022-10-31 21:53:37 -0400

    Create lr_sche.py

---
+++ lr_sche.py
@@ -0,0 +1,103 @@
+import argparse
+import os
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim
+import torch.utils.data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+from torch.autograd import Variable
+
+from utils.wideresnet import WideResNet
+from torch.utils.data import DataLoader
+from tqdm import tqdm
+
+# used for logging to TensorBoard
+from tensorboard_logger import configure, log_value
+
+parser = argparse.ArgumentParser(description='WideResNet Pre-Training')
+# training specs
+parser.add_argument('--dataset', default='cifar100', type=str,
+                    help='dataset (cifar10 or cifar100)')
+parser.add_argument('--epochs', default=200, type=int,
+                    help='number of total epochs')
+parser.add_argument('--start-epoch', default=0, type=int,
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('-b', '--batch-size', default=128, type=int,
+                    help='mini-batch size (default: 128)')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    help='initial learning rate')
+# model specs
+parser.add_argument('--layers', default=28, type=int,
+                    help='total number of layers (default: 28)')
+parser.add_argument('--widen-factor', default=10, type=int,
+                    help='widen factor (default: 10)')
+parser.add_argument('--droprate', default=0.3, type=float,
+                    help='dropout probability')
+# logging specs
+parser.add_argument('--print-freq', '-p', default=10, type=int,
+                    help='print frequency (default: 10)')
+parser.add_argument('--resume', default='', type=str,
+                    help='path to latest checkpoint (default: none)')
+parser.add_argument('--name', default='WideResNet-28-10', type=str,
+                    help='name of experiment')
+parser.add_argument('--tensorboard',
+                    help='Log progress to TensorBoard', action='store_true')
+
+best_acc1 = 0
+
+def main():
+    global args, best_acc1
+    args = parser.parse_args()
+    if args.tensorboard: configure("runs/%s"%(args.name))
+
+    # data loading
+    train_transform = transforms.Compose([
+      transforms.ToTensor(),
+      transforms.Lambda(lambda x: F.pad(x.unsqueeze(0), (4,4,4,4),mode='reflect').squeeze()),
+        transforms.ToPILImage(),
+        transforms.RandomCrop(32),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize([0.5071, 0.4865, 0.4409], [0.2673, 0.2564, 0.2762])
+        ])
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize([0.5071, 0.4865, 0.4409], [0.2673, 0.2564, 0.2762])
+        ])
+
+    train_loader = DataLoader(datasets.__dict__[args.dataset.upper()](root='./data', train=True, transform=train_transform, download=True),
+        batch_size=args.batch_size, shuffle=True, num_workers=16, pin_memory=True)
+    val_loader = DataLoader(datasets.__dict__[args.dataset.upper()](root='./data', train=False, transform=test_transform, download=True),
+        batch_size=args.batch_size, shuffle=False, num_workers=16, pin_memory=True)
+
+ 
+
+    cudnn.benchmark = True
+    # training specs
+    criterion = nn.CrossEntropyLoss().cuda()
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, len(train_loader)*args.epochs)
+    
+    lr_list = []
+    torch.save( lr_list, './runs/save_data/lr_sche.pt' )
+    for epoch in range(args.start_epoch, args.epochs):
+        progress = tqdm(train_loader)
+        if (epoch+1) in [1, 2, 5, 10, 25, 50, 75, 100, 150, 200]:
+            lr = scheduler.get_last_lr()
+            lr_list.append( lr )
+            print(epoch+1, lr, 'lr')
+        for input, target in progress:
+            scheduler.step()
+    
+    torch.save( lr_list, './runs/save_data/lr_sche.pt' )
+  
+
+
+if __name__ == '__main__':
+    main()
